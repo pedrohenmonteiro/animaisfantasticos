0d3c4ff90b11c135c3c06d13f45fd22a48bf51a6
pedrohenmonteiro <pedromont436@gmail.com>
2022-01-06 21:15:36 -0300

    navegacao por tabs

--- script.js
+++ script.js
@@ -1,51 +1,17 @@
-// Quando o usuário clicar nos links internos do site,
-// adicione a classe ativo ao item clicado e remova dos
-// demais itens caso eles possuam a mesma. Previna
-// o comportamento padrão desses links
-const links = document.querySelectorAll("a[href^='#'");
+const tabMenu = document.querySelectorAll(".js-tabmenu li");
+const tabContent = document.querySelectorAll(".js-tabcontent section");
 
-console.log(links);
+if (tabMenu.length && tabContent.length) {
+  tabContent[0].classList.add("ativo");
 
-function handleLink(event) {
-  event.preventDefault();
-  links.forEach((link) => {
-    link.classList.remove("ativo");
-  });
-  event.currentTarget.classList.add("ativo");
-}
-
-links.forEach((link) => {
-  link.addEventListener("click", handleLink);
-});
-
-// Selecione todos os elementos do site começando a partir do body,
-const todosElementos = document.querySelectorAll("body *");
-console.log(todosElementos);
-// ao clique mostre exatamente quais elementos estão sendo clicados
-function evento(event) {
-  event.target.remove();
-}
-
-todosElementos.forEach(() => {
-  addEventListener("click", evento);
-});
-
-// Utilizando o código anterior, ao invés de mostrar no console,
-// remova o elemento que está sendo clicado, o método remove() remove um elemento
-
-// Se o usuário clicar na tecla (t), aumente todo o texto do site.
-
-const body = document.querySelector("body");
-
-body.style.fontSize === "100%";
-function handleKey(event) {
-  if (event.key === "t") {
-    if (body.style.fontSize !== "150%") {
-      body.style.fontSize = "150%";
-    } else {
-      body.style.fontSize = "100%";
-    }
+  function activeTab(index) {
+    tabContent.forEach((item) => item.classList.remove("ativo"));
+    tabContent[index].classList.add("ativo");
   }
-}
 
-addEventListener("keydown", handleKey);
+  tabMenu.forEach((itemMenu, index) => {
+    itemMenu.addEventListener("click", () => {
+      activeTab(index);
+    });
+  });
+}
