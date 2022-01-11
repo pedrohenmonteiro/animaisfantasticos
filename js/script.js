a78f8e6ff5ecbf1f95fecf6db2792be6aa5a9a9c
pedrohenmonteiro <pedromont436@gmail.com>
2022-01-11 00:10:47 -0300

    modal

---
+++ js/script.js
@@ -0,0 +1,11 @@
+import initTabNav from "./modules/tabnav.js";
+import initAccordion from "./modules/accordion.js";
+import initScrollSuave from "./modules/scroll-suave.js";
+import initAnimacaoScroll from "./modules/scroll-animacao.js";
+import initModal from "./modules/modal.js";
+
+initTabNav();
+initAccordion();
+initScrollSuave();
+initAnimacaoScroll();
+initModal();
