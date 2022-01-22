35bac5e784e267708e26b06019ebee4c6cec944a
pedrohenmonteiro <pedromont436@gmail.com>
2022-01-22 00:08:42 -0300

    accordion refatorado em classe

--- js/script.js
+++ js/script.js
@@ -1,12 +1,11 @@
 import initTabNav from "./modules/tabnav.js";
-import initAccordion from "./modules/accordion.js";
+import Accordion from "./modules/accordion.js";
 import ScrollSuave from "./modules/scroll-suave.js";
 import initAnimacaoScroll from "./modules/scroll-animacao.js";
 import initModal from "./modules/modal.js";
 import initTooltip from "./modules/tooltip.js";
 import initDropdownMenu from "./modules/dropdown-menu.js";
 import initMenuMobile from "./modules/menu-mobile.js";
-
 import initFuncionamento from "./modules/funcionamento.js";
 import initFetchAnimais from "./modules/fetch-animais.js";
 import initFetchBitcoin from "./modules/fetch-bitcoin.js";
@@ -14,8 +13,10 @@ import initFetchBitcoin from "./modules/fetch-bitcoin.js";
 const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
 scrollSuave.init();
 
+const accordion = new Accordion("[data-anime='accordion'] dt");
+accordion.init();
+
 initTabNav();
-initAccordion();
 initAnimacaoScroll();
 initModal();
 initTooltip();
